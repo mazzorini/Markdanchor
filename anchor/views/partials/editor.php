fe7643b5df38f4e8dd90d8f14146f01baf697ac4
Marcel Gurski <marcelgurski@gmail.com>
2014-02-24 21:58:09 +0100

    Add Markdanchor

---
+++ anchor/views/partials/editor.php
@@ -0,0 +1,23 @@
+<!-- FA Fonts -->
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script> 
+
+<nav class="btn-group">
+    <a class="btn btn-default fa fa-bold" href="#bold"></a>
+    <a class="btn btn-default fa fa-italic" href="#italic"></a>
+    <!-- not implemented in Anchor 0.9.2 <a class="btn btn-default fa fa-strikethrough" href="#strike"></a> -->
+    <a class="btn btn-default fa fa-list-ul" href="#list"></a>
+    <a class="btn btn-default fa fa-quote-right" href="#quote"></a>
+    <a class="btn btn-default fa fa-code" href="#code"></a>
+    <a class="btn btn-default fa fa-link" href="#link"></a>
+    <a class="btn btn-default fa fa-picture-o" href="#img"></a>
+    <a class="btn btn-default" href="#h1">H1</a>
+    <a class="btn btn-default" href="#h2">H2</a>
+    <a class="btn btn-default" href="#h3">H3</a>
+    <a class="btn btn-default" href="#h4">H4</a>
+    <a class="btn btn-default" href="#h5">H5</a>
+    <a class="btn btn-default" href="#h6">H6</a>
+</nav>
